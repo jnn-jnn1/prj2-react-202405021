75c6063aea03ed8b311506d81bb01665a0b8b95e
Jin Jin <anj72005@gmail.com>
2024-05-21 15:11:57 +0900

    한 게시물 보기, Spinner

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -1,11 +1,53 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner,
+  Textarea,
+} from "@chakra-ui/react";
 
 export function BoardView() {
+  const [board, setBoard] = useState(null);
   const { id } = useParams();
   useEffect(() => {
-    axios.get(`/api/board/${id}`).then();
+    axios.get(`/api/board/${id}`).then((res) => setBoard(res.data));
   }, []);
-  return null;
+
+  if (board === null) {
+    return <Spinner />;
+  }
+
+  return (
+    <Box>
+      <Box>{board.id}번 게시물</Box>
+      <Box>
+        <FormControl>
+          <FormLabel>제목</FormLabel>
+          <Input value={board.title} readOnly />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>본문</FormLabel>
+          <Textarea value={board.content} readOnly />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>작성자</FormLabel>
+          <Input value={board.writer} readOnly />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>작성일시</FormLabel>
+          <Input type={"datetime-local"} value={board.inserted} readOnly />
+        </FormControl>
+      </Box>
+    </Box>
+  );
 }
