25fe894e598a5d4b1a7ded112d9927f65bff9c8d
Jin Jin <anj72005@gmail.com>
2024-05-27 09:45:24 +0900

    useSearchParams 이용해서 페이지 받고 페이지 번호 클릭시 해당 페이지 게시물 목록 출력

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -1,17 +1,22 @@
-import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserPen } from "@fortawesome/free-solid-svg-icons/faUserPen";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
   useEffect(() => {
-    axios.get("/api/board/list").then((res) => setBoardList(res.data));
-  }, []);
+    axios
+      .get(`/api/board/list?${searchParams}`)
+      .then((res) => setBoardList(res.data));
+  }, [searchParams]);
+
+  console.log(searchParams.toString());
 
   return (
     <Box>
@@ -42,6 +47,16 @@ export function BoardList() {
             ))}
           </Tbody>
         </Table>
+      </Box>
+      <Box>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((pageNumber) => (
+          <Button
+            key={pageNumber}
+            onClick={() => navigate(`/?page=${pageNumber}`)}
+          >
+            {pageNumber}
+          </Button>
+        ))}
       </Box>
     </Box>
   );
