574802333cc0e32ae9d6f943427521aef57b7963
Jin Jin <anj72005@gmail.com>
2024-05-27 15:17:45 +0900

    주소 복사 후 새 탭에서 이동시 input에 검색기록 남기기

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -42,6 +42,16 @@ export function BoardList() {
       setBoardList(res.data.boardList);
       setPageInfo(res.data.pageInfo);
     });
+
+    const typeParam = searchParams.get("type");
+    const keywordParam = searchParams.get("keyword");
+
+    if (typeParam) {
+      setSearchType(typeParam);
+    }
+    if (keywordParam) {
+      setSearchKeyword(keywordParam);
+    }
   }, [searchParams]);
 
   function handleSearchClick() {
@@ -84,7 +94,10 @@ export function BoardList() {
       <Center>
         <Flex>
           <Box>
-            <Select onChange={(e) => setSearchType(e.target.value)}>
+            <Select
+              value={searchType}
+              onChange={(e) => setSearchType(e.target.value)}
+            >
               <option value={"all"}>전체</option>
               <option value={"text"}>글</option>
               <option value={"nickName"}>작성자</option>
@@ -92,6 +105,7 @@ export function BoardList() {
           </Box>
           <Box>
             <Input
+              value={searchKeyword}
               onChange={(e) => setSearchKeyword(e.target.value)}
               placeholder={"검색어"}
             />
