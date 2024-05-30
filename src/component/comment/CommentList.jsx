b46fb18fb2dd47f55a3da2a14e5343ce0e470168
Jin Jin <anj72005@gmail.com>
2024-05-30 11:17:47 +0900

    댓글 목록

--- src/component/comment/CommentList.jsx
+++ src/component/comment/CommentList.jsx
@@ -1,3 +1,33 @@
-export function CommentList() {
-  return null;
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Box, Spacer } from "@chakra-ui/react";
+
+export function CommentList({ boardId }) {
+  const [commentList, setCommentList] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`/api/comment/list/${boardId}`)
+      .then((res) => setCommentList(res.data));
+  }, []);
+  if (commentList.length === 0) {
+    return <Box>댓글이 없습니다. 첫 댓글을 작성해보세요</Box>;
+  }
+  return (
+    <Box>
+      {commentList.map((comment) => (
+        <Box key={comment.id} border={"1px solid black"} my={3}>
+          <Box>
+            <Box>{comment.memberId}</Box>
+          </Box>
+          <Spacer />
+          <Box>
+            <Box>{comment.inserted}</Box>
+          </Box>
+          <Box>
+            <Box>{comment.comment}</Box>
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  );
 }
