00bb9d280afdbd1b264463b59f1a43370765e8de
Jin Jin <anj72005@gmail.com>
2024-05-30 16:31:29 +0900

    댓글 삭제 시 모달창 띄우기

--- src/component/comment/CommentItem.jsx
+++ src/component/comment/CommentItem.jsx
@@ -1,9 +1,24 @@
-import { Box, Button, Flex, Spacer } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 import axios from "axios";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export function CommentItem({ comment, isProcessing, setIsProcessing }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
   function handleRemoveClick() {
     setIsProcessing(true);
     axios
@@ -11,7 +26,15 @@ export function CommentItem({ comment, isProcessing, setIsProcessing }) {
         data: { id: comment.id },
       })
       .then()
-      .finally(() => setIsProcessing(false));
+      .finally(() => {
+        setIsProcessing(false);
+        onClose();
+        toast({
+          status: "success",
+          description: "댓글이 삭제되었습니다",
+          position: "top",
+        });
+      });
   }
 
   return (
@@ -28,11 +51,28 @@ export function CommentItem({ comment, isProcessing, setIsProcessing }) {
           <Box>{comment.comment}</Box>
         </Box>
         <Box>
-          <Button isLoading={isProcessing} onClick={handleRemoveClick}>
+          <Button isLoading={isProcessing} onClick={onOpen}>
             <FontAwesomeIcon icon={faTrash} />
           </Button>
         </Box>
       </Flex>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>삭제 확인</ModalHeader>
+          <ModalBody>댓글을 삭제하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>취소</Button>
+            <Button
+              colorSheme={"red"}
+              isLoading={isProcessing}
+              onClick={handleRemoveClick}
+            >
+              확인
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
