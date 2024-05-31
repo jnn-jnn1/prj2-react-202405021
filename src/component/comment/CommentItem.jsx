fda6c3c0eca23a0dbb9ae5b49e99c25166c9f0f0
Jin Jin <anj72005@gmail.com>
2024-05-31 09:31:26 +0900

    자신의 댓글만 사제 가능

--- src/component/comment/CommentItem.jsx
+++ src/component/comment/CommentItem.jsx
@@ -15,25 +15,37 @@ import {
 import axios from "axios";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { LoginContext } from "../LoginProvider.jsx";
+import { useContext } from "react";
 
 export function CommentItem({ comment, isProcessing, setIsProcessing }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const toast = useToast();
+  const account = useContext(LoginContext);
+
   function handleRemoveClick() {
     setIsProcessing(true);
     axios
       .delete("/api/comment/remove", {
         data: { id: comment.id },
       })
-      .then()
-      .finally(() => {
-        setIsProcessing(false);
-        onClose();
+      .then(() =>
         toast({
           status: "success",
           description: "댓글이 삭제되었습니다",
           position: "top",
-        });
+        }),
+      )
+      .catch(() =>
+        toast({
+          status: "error",
+          description: "권한이 없습니다",
+          position: "top",
+        }),
+      )
+      .finally(() => {
+        setIsProcessing(false);
+        onClose();
       });
   }
 
@@ -47,32 +59,35 @@ export function CommentItem({ comment, isProcessing, setIsProcessing }) {
         <Box>{comment.inserted}</Box>
       </Box>
       <Flex>
-        <Box>
-          <Box>{comment.comment}</Box>
-        </Box>
-        <Box>
-          <Button isLoading={isProcessing} onClick={onOpen}>
-            <FontAwesomeIcon icon={faTrash} />
-          </Button>
-        </Box>
-      </Flex>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>삭제 확인</ModalHeader>
-          <ModalBody>댓글을 삭제하시겠습니까?</ModalBody>
-          <ModalFooter>
-            <Button onClick={onClose}>취소</Button>
-            <Button
-              colorSheme={"red"}
-              isLoading={isProcessing}
-              onClick={handleRemoveClick}
-            >
-              확인
+        <Box>{comment.comment}</Box>
+        <Spacer />
+        {account.hasAccess(comment.memberId) && (
+          <Box>
+            <Button isLoading={isProcessing} onClick={onOpen}>
+              <FontAwesomeIcon icon={faTrash} />
             </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+          </Box>
+        )}
+      </Flex>
+      {account.hasAccess(comment.memberId) && (
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>삭제 확인</ModalHeader>
+            <ModalBody>댓글을 삭제하시겠습니까?</ModalBody>
+            <ModalFooter>
+              <Button onClick={onClose}>취소</Button>
+              <Button
+                colorSheme={"red"}
+                isLoading={isProcessing}
+                onClick={handleRemoveClick}
+              >
+                확인
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      )}
     </Box>
   );
 }
