673b96db519c3ba16c482cf0314cf920efe40514
Jin Jin <anj72005@gmail.com>
2024-05-31 11:23:38 +0900

    댓글 수정 전 모달, 댓글 수정 후 토스트

--- src/component/comment/CommentEdit.jsx
+++ src/component/comment/CommentEdit.jsx
@@ -1,4 +1,17 @@
-import { Box, Button, Flex, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Textarea,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
@@ -11,12 +24,20 @@ export function CommentEdit({
   setIsProcessing,
 }) {
   const [commentText, setCommentText] = useState(comment.comment);
+  const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   function handleCommentSubmit() {
     setIsProcessing(true);
     axios
       .put("/api/comment/edit", { id: comment.id, comment: commentText })
-      .then()
+      .then(() =>
+        toast({
+          status: "success",
+          description: "댓글이 수정되었습니다",
+          position: "top",
+        }),
+      )
       .finally(() => {
         setIsProcessing(false);
         setIsEditing(false);
@@ -43,11 +64,24 @@ export function CommentEdit({
           isLoading={isProcessing}
           variant={"ghost"}
           colorScheme={"blue"}
-          onClick={handleCommentSubmit}
+          onClick={onOpen}
         >
           <FontAwesomeIcon icon={faPaperPlane} />
         </Button>
       </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>수정 확인</ModalHeader>
+          <ModalBody>댓글을 저장하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>취소</Button>
+            <Button colorScheme={"blue"} onClick={handleCommentSubmit}>
+              확인
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 }
